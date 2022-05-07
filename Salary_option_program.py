91cacab8f1a91759e7046f83c6d3dc4ed359f3ad
calivee <calivee.ca@gmail.com>
2022-05-07 02:17:41 -0700

    second commit

--- Salary_option_program.py
+++ Salary_option_program.py
@@ -11,3 +11,19 @@ end
 """
 
 """
+Pseudo Code
+initialize total to 0
+initialize counter to 0
+get list of grades from txt. file (open in read (r))
+input first grade
+add this grade to total
+add one to the grade counter
+input the next grade
+if counter is not = 0
+set the average to the total divided by the counter
+print the number of grades
+print the average
+print the percent above the average
+"""
+
+
