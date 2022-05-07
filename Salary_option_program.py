86c1ef1b980665b0410a9f8cc8a3fe2bcda11b58
calivee <calivee.ca@gmail.com>
2022-05-07 02:10:36 -0700

    second commit

---
+++ Salary_option_program.py
@@ -0,0 +1,13 @@
+"""
+PLAIN ENGLISH
+open file in read
+capture variable (test scores) from list of grades (float)
+calculate number of grades
+append grade scores to avg lis
+calculate average class grade score from avg list of grades 
+calculate number of above average scores
+out grade information to user
+end
+"""
+
+"""
